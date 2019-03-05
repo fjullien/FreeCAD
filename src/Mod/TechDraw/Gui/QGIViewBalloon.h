8986f8ad825ae032f5cd49d28b2a7d4bbef6ece1
Franck Jullien <franck.jullien@odyssee-systemes.fr>
2019-03-05 13:32:31 +0100

    WIP: TechDraw: remove unused Balloon findIsoDir and findIsoExt

--- src/Mod/TechDraw/Gui/QGIViewBalloon.h
+++ src/Mod/TechDraw/Gui/QGIViewBalloon.h
@@ -87,8 +87,6 @@ protected:
                                  const QVariant &value ) override;
     virtual void setSvgPens(void);
     virtual void setPens(void);
-    Base::Vector3d findIsoDir(Base::Vector3d ortho);
-    Base::Vector3d findIsoExt(Base::Vector3d isoDir);
     QString getPrecision(void);
 
 protected:
